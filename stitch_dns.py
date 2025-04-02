675d7451773ac802c8698be77ffad42c9afec603
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2025-04-03 01:47:32 +0200

    Add script to stich DNS uniflow to biflows
    The ipfixprobe tool exports DNS as uniflows even though the rest is
    exported as biflow when the `dns` plugin is used.
    
    The script first aggregates the uniflows by the 5-tuple and family,
    sample, DNS_ID, DNS_NAME. These were found to uniquely represent a DNS
    uniflow (confirmed in Wireshark).
    Then the incoming uniflows (host <- dns server) have their reversed
    columns swapped (e.g. SRC_IP and DST_IP).
    Finally, the uniflows are aggregated into biflows.
    
    NOTE: Some columns (e.g. the binary ones - TCP_FLAGS) have to be removed
    as their aggregation is not yet implemented.

---
+++ stitch_dns.py
@@ -0,0 +1,112 @@
+import itertools
+import sys
+from ast import literal_eval
+
+import pandas as pd
+
+_STITCH_COLS = ["family", "sample", "DNS_ID", "DNS_NAME"]
+_TUPLE_COLS = [
+    "SRC_IP",
+    "SRC_PORT",
+    "DST_IP",
+    "DST_PORT",
+    "PROTOCOL",
+]
+
+def concat_lists(lists):
+    return list(itertools.chain(*lists))
+
+_agg_funcs = {
+    "BYTES": "sum",
+    "BYTES_REV": "sum",
+    "PACKETS": "sum",
+    "PACKETS_REV": "sum",
+    "TIME_FIRST": "min",
+    "TIME_LAST": "max",
+    # "PROTOCOL": "first",
+    "PPI_PKT_LENGTHS": concat_lists,
+    "PPI_PKT_TIMES": concat_lists,
+}
+
+
+def stitch_dns(df):
+    """
+    Stitch DNS records together based on the family, sample, DNS_ID, and DNS_NAME.
+    """
+    ordered_cols = [
+        "SRC_IP",
+        "DST_IP",
+        "SRC_PORT",
+        "DST_PORT",
+        "BYTES",
+        "BYTES_REV",
+        "PACKETS",
+        "PACKETS_REV",
+    ]
+    reverse_cols = [
+        "DST_IP",
+        "SRC_IP",
+        "DST_PORT",
+        "SRC_PORT",
+        "BYTES_REV",
+        "BYTES",
+        "PACKETS_REV",
+        "PACKETS",
+    ]
+    groupcols = _STITCH_COLS + _TUPLE_COLS
+
+    def flip_reversed_flows(group):
+        # NOTE: hopefully there is no weird case where the first packet is not sent by the host
+        # this maybe could happen if the capture was start after the request and before the response
+        group = group.sort_values(by="TIME_FIRST")
+        hostip = group.iloc[0]["SRC_IP"]
+
+        # swap the reversed columns
+        isreversed = group["DST_IP"] == hostip
+        # NOTE: this is ugly but recommended in the pandas docs
+        # > The correct way to swap column values is by using raw values
+        group.loc[isreversed, ordered_cols] = group.loc[isreversed, reverse_cols].values
+        return group
+
+
+    # first aggregate the flows in the same direction
+    df = df.groupby(groupcols).agg(_agg_funcs).reset_index()
+    print("Uniflow Aggregated:")
+    print(df)
+    print(df.index)
+    print(df.columns)
+
+    # 
+    df = df.groupby(_STITCH_COLS, group_keys=False).apply(flip_reversed_flows)
+    print("Reversed columns:")
+    print(df)
+    print(df.index)
+    print(df.columns)
+
+    print('Reset index')
+    df = df.reset_index()
+    print(df.index)
+    print(df.columns)
+
+    # agg to biflow
+    df = df.groupby(groupcols).agg(_agg_funcs)
+    print("Reversed columns and biflow merge:")
+    print(df)
+    return df
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(sys.argv[1])
+
+    df = parse_ppi(df)
+
+    # lets drop some unused columns
+    keep_cols = set(_agg_funcs.keys()) | set(_STITCH_COLS) | set(_TUPLE_COLS)
+    df = df[list(keep_cols)]
+
+    dns = df[df["DNS_NAME"].notna()]
+    nondns = df[df["DNS_NAME"].isna()]
+
+    dns = stich_dns(dns)
+    print("Final DNS:")
+    pd.concat([nondns, dns]).to_csv("stitched.csv", index=True)
