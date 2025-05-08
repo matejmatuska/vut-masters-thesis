f79d13a8e9b0c3e209eaef620a3f2784c0e776ec
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2025-05-08 21:23:48 +0200

    clean_dataset.py: Rafactor and cleanup

--- clean_dataset.py
+++ clean_dataset.py
@@ -6,7 +6,14 @@ import pandas as pd
 _DNS_PTR_QTYPE = 12
 
 
-def load_dataset(path) -> pd.DataFrame:
+def load_csvs_dataset(path) -> pd.DataFrame:
+    """
+    Load the CSVs dataset from the given path
+
+    :param path: path to the dataset
+    :return: the loaded dataset
+    :rtype: pd.DataFrame
+    """
     dfs = []
 
     for subdir, _, files in os.walk(path):
@@ -31,7 +38,18 @@ def load_dataset(path) -> pd.DataFrame:
     return dataset
 
 
-def get_host_ip(df):
+def sample_get_host_ip(df) -> pd.Series:
+    """
+    Find host IP in a sample
+
+    Host IP is the one that occurs in every flow of the sample, either as
+    source or destination IP.
+
+    :param df: the sample
+    :type df: pd.DataFrame
+    :return: the found host IP
+    :rtype: pd.Series
+    """
     all_ips = pd.concat([df["SRC_IP"], df["DST_IP"]])
     ip_counts = all_ips.value_counts()
 
@@ -40,17 +58,26 @@ def get_host_ip(df):
     return pd.Series({"host_ip": ip_in_every_flow})
 
 
-def get_common_ips(df):
+def get_common_ips(df) -> pd.Series:
+    """
+    Get IPs common IPs across multiple families
+
+    :param df: the dataset
+    :type df: pd.DataFrame
+    :return: a list of common IPs
+    :rtype: pd.Series
+    """
     unique_dst_ips = df.groupby(["family", "sample"])["DST_IP"].unique()
     host_ips = df.groupby(["family", "sample"], group_keys=True)[
         ["SRC_IP", "DST_IP"]
-    ].apply(get_host_ip)
+    ].apply(sample_get_host_ip)
 
     unique_dst_ips = pd.merge(
         unique_dst_ips, host_ips, how="outer", on=["family", "sample"]
     ).explode("DST_IP")
     # filtered_unique_dst_ips.to_csv('hosts_and_dsts.csv')
 
+    # do not remove the host IPs
     before = len(unique_dst_ips)
     unique_dst_ips = unique_dst_ips[
         unique_dst_ips["DST_IP"] != unique_dst_ips["host_ip"]
@@ -65,16 +92,27 @@ def get_common_ips(df):
     # result.to_csv('uniq_dst_ips.v2.csv', index=False)
 
 
-def filter_common_ips(df, common_ips):
+def filter_common_ips(df, common_ips) -> pd.DataFrame:
+    """
+    Filter common IPs from the dataset
+    :param df: the dataset
+    :param common_ips: the common IPs to filter out
+    :return: the filtered dataset
+    :rtype: pd.DataFrame
+    """
     return df[
         (~df["DST_IP"].isin(set(common_ips))) & (~df["SRC_IP"].isin(set(common_ips)))
     ]
 
 
-def filter_DNS(df, filter_names, output_dir):
-    top1m = pd.read_csv(filter_names, names=["index", "domain"], usecols=["domain"])[
-        "domain"
-    ]
+def load_and_filter_top1m(top1m_path) -> pd.DataFrame:
+    """
+    Load the top-1m domains and remove the ones that could be used maliciously
+
+    :param top1m_path: path to the original top-1m domains file
+    :return: the filtered top-1m domains
+    :rtype: pd.DataFrame
+    """
     keep_domains = [
         r"^mail\..*",
         r"^smtp\..*",
@@ -137,23 +175,35 @@ def filter_DNS(df, filter_names, output_dir):
     ]
     # htlb.casalemedia.com
     # TODO keep dns server names?
-
     combined_regex = "|".join(f"({pattern})" for pattern in keep_domains)
-    top1m = top1m[~top1m.str.contains(combined_regex, regex=True)]
-    top1m.to_csv(
-        os.path.join(output_dir, "domains-to-remove.csv"), index=False
-    )  # TODO put to some dated folder
 
-    index = df["DNS_NAME"].isin(set(top1m))
-
-    removed = df[index]
-    removed["DNS_NAME"].to_csv(os.path.join(output_dir, "removed_dns.csv"), index=False)
-    return df[~index]
+    top1m = pd.read_csv(top1m_path, names=["index", "domain"], usecols=["domain"])[
+        "domain"
+    ]
+    return top1m[~top1m.str.contains(combined_regex, regex=True)]
 
 
-def filter_rDNS(df, output_dir) -> pd.DataFrame:
+def filter_DNS(df, filter) -> (pd.DataFrame, pd.Series):
     """
-    Filter reverse DNS requests occurring in multiple families
+    Filter DNS requests present in filter
+
+    :param df: the dataset
+    :param filter: List of domains to filter out
+    :return: the filtered dataset and a series of removed domains
+    :rtype: (pd.DataFrame, pd.Series)
+    """
+    index = df["DNS_NAME"].isin(set(filter))
+    removed = df[index]
+    return (df[~index], removed["DNS_NAME"])
+
+
+def filter_rDNS(df) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Filter reverse DNS requests occurring among multiple families
+
+    :param df: the dataset DataFrame
+    :return: the filtered dataset and the removed dataset
+    :rtype: (pd.DataFrame, pd.DataFrame)
     """
     df["DNS_NAME"] = df["DNS_NAME"].map(
         lambda x: x.rstrip(".in-addr.arpa"), na_action="ignore"
@@ -165,24 +215,33 @@ def filter_rDNS(df, output_dir) -> pd.DataFrame:
     index = (df["DNS_QTYPE"] == _DNS_PTR_QTYPE) & (
         df["DNS_NAME"].isin(set(common_names[common_names > 1].index))
     )
-
-    removed = df[index]
-    removed.to_csv(os.path.join("removed_rdns.csv"), index=False)
-    return df[~index]
+    return (df[~index], d[index])
 
 
 def run(dset_path, output_dir) -> pd.DataFrame:
+    # inputs
+    top1m_path = os.path.join("data", "top-1m.csv")  # TODO find some place for this
+    common_ips_path = os.path.join(
+        "data", "filter_common_ips.csv"
+    )  # TODO find some place for this
+
+    # outputs
+    top1m_mod_path = os.path.join(output_dir, "domains-to-remove.csv")
+    removed_dns_path = os.path.join(output_dir, "removed_dns.csv")
+    removed_rdns_path = os.path.join(output_dir, "removed_rdns.csv")
+
     print("Loading dataset")
-    df = load_dataset(dset_path)
+    df = load_csvs_dataset(dset_path)
     print(f"Loaded {len(df)} rows (flows)")
-    print(df.columns)
 
     prev_len = len(df)
 
     print("Filtering common IPs")
-    common_ips = pd.read_csv(
-        "data/filter_common_ips.csv"
-    )  # TODO: this should be computed!!!
+    if True:  # TODO: this should be computed!!!
+        common_ips = pd.read_csv(common_ips_path)
+    else:
+        common_ips = get_common_ips(df)
+        # common_ips.to_csv(common_ips_path, index=False)
     df = filter_common_ips(df, common_ips["DST_IP"])
     # print(df.groupby(['family', 'sample']).size())
     print(f"Removed {prev_len - len(df)} rows")
@@ -190,13 +249,17 @@ def run(dset_path, output_dir) -> pd.DataFrame:
     prev_len = len(df)
 
     print("Filtering DNS requests")
-    df = filter_DNS(df, "data/top-1m.csv", output_dir)
+    top1m = load_and_filter_top1m(top1m_path)
+    top1m.to_csv(top1m_mod_path, index=False)
+    df, removed_domains = filter_DNS(df, top1m)
+    removed_domains.to_csv(removed_dns_path, index=False)
     print(f"Removed {prev_len - len(df)} rows")
 
     prev_len = len(df)
 
     print("Filtering common rDNS requests")
-    df = filter_rDNS(df, output_dir)
+    df, removed = filter_rDNS(df)
+    removed.to_csv(removed_rdns_path, index=False)
     print(f"Removed {prev_len - len(df)} rows")
     return df
 
@@ -207,5 +270,14 @@ if __name__ == "__main__":
         sys.exit(1)
 
     path = sys.argv[1]
-    df = run(path, sys.argv[2])
-    df.to_csv(os.path.join(sys.argv[2], "dataset.csv"), index=False)
+    if not os.path.exists(path):
+        print(f"Dataset path {path} does not exist")
+        sys.exit(1)
+
+    output_dir = sys.argv[2]
+    if not os.path.exists(output_dir):
+        print(f"Output directory {output_dir} does not exist")
+        sys.exit(1)
+
+    df = run(path, output_dir)
+    df.to_csv(os.path.join(output_dir, "dataset.csv"), index=False)
