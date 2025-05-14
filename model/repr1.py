383665092c1320836224747c7cda4c40dd8783df
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2025-05-14 20:34:45 +0200

    repr1: Use dummy features

--- model/repr1.py
+++ model/repr1.py
@@ -14,11 +14,9 @@ class Repr1Classifier(torch.nn.Module):
     def __init__(self, input_dim, num_hosts, hidden_dim, num_classes, layers, dropout=0.0):
         super().__init__()
         self.dropout = dropout
-        # Host has no features - use embedding
-        self.host_embed = torch.nn.Embedding(4000, hidden_dim)
+        self.hidden_dim = hidden_dim
 
-        # data['Host'].x = torch.ones((num_host_nodes, feature_dim))
-        #self.lin_flow = Linear(input_dim, hidden_dim)
+        # self.host_embed = torch.nn.Embedding(4000, hidden_dim)
 
         self.convs = torch.nn.ModuleList()
         self.convs.append(HeteroConv({
@@ -35,7 +33,7 @@ class Repr1Classifier(torch.nn.Module):
             }, aggr='sum'))
 
         self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(hidden_dim, hidden_dim // 2),
+            torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.ReLU(),
             torch.nn.Dropout(self.dropout),
             torch.nn.Linear(hidden_dim, hidden_dim),
@@ -45,10 +43,10 @@ class Repr1Classifier(torch.nn.Module):
         )
 
     def forward(self, data):
-        # Embed host IDs (assumes they are stored in data['Host'].node_ids)
-        x_host = self.host_embed(data['Host'].node_ids)
+        # x_host = self.host_embed(data['Host'].node_ids)
 
-        #x_host = torch.zeros((data.num_nodes, 1))  # dummy node features
+        # dummy node features
+        x_host = torch.zeros((data.num_nodes, self.hidden_dim)).to(data.y.device)
         #x_flow = self.lin_flow(data['NetworkFlow'].x)
 
         x_dict = {'Host': x_host, 'NetworkFlow': data['NetworkFlow'].x}
@@ -63,7 +61,7 @@ class Repr1Classifier(torch.nn.Module):
 
         x_dict = self.convs[-1](x_dict, data.edge_index_dict)
 
-        #pooled_host = global_mean_pool(x_dict['Host'], data['Host'].batch)
+        # pooled_host = global_mean_pool(x_dict['Host'], data['Host'].batch)
         pooled_flow = global_max_pool(x_dict['NetworkFlow'], data['NetworkFlow'].batch)
 
         #out = self.classifier(torch.cat([pooled_host, pooled_flow], dim=1))
