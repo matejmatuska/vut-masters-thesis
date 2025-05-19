df095aac25013b609cc6ecd46e15a2b3089b8056
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2025-05-19 02:14:58 +0200

    Switch repr1 to GCN

--- model/repr1.py
+++ model/repr1.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GraphConv, global_max_pool
+from torch_geometric.nn import GCNConv, global_max_pool
 
 TOTAL_PORTS = 65536  # 2^16
 TCP_FLAGS_VALUES = 256  # 8 bit representation
@@ -19,19 +19,19 @@ class Repr1Classifier(torch.nn.Module):
         input_dim = input_dim + tcp_flags_dim + port_dim
 
         self.convs = torch.nn.ModuleList()
-        self.convs.append(GraphConv(input_dim, hidden_dim))
+        self.convs.append(GCNConv(input_dim, hidden_dim))
         for _ in range(layers - 2):
-            self.convs.append(GraphConv(hidden_dim, hidden_dim))
-        self.convs.append(GraphConv(hidden_dim, hidden_dim))
+            self.convs.append(GCNConv(hidden_dim, hidden_dim))
+        self.convs.append(GCNConv(hidden_dim, hidden_dim))
 
         self.fc = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(self.dropout),
             torch.nn.Linear(hidden_dim, hidden_dim // 2),
             torch.nn.ReLU(),
             torch.nn.Dropout(self.dropout),
-            torch.nn.Linear(hidden_dim // 2, hidden_dim // 4),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(self.dropout),
-            torch.nn.Linear(hidden_dim // 4, num_classes),
+            torch.nn.Linear(hidden_dim // 2, num_classes),
         )
 
     def forward(self, data):
